14688f28d6a4195b70c41968de2f2b88e41439fa
B-V-GOWTHAM <balureddyvelayudhamgowtham@gmail.com>
2025-03-24 17:48:17 +0530

    Square code updated

--- Square.java
+++ Square.java
@@ -1,23 +1,45 @@
 import java.util.*;
-class square{
+
+public class Square{
     int side;
-    square()
-    {
-        side = 0;
+    Square(){
+        side=0;
     }
-    
-    int getArea()
-    {
-        return side * side;
+    Square(int side){
+        this.side=side;
     }
+    void area(){
+        System.out.print("Area of the Square :"+(side*side));
+    }
+    void perimeter(){
+        System.out.print("Perimeter of the Square :"+(4*side));
+    }
+    void volume(){
+        System.out.print("Volume of the Square :"+(side*side*side));
+    }
+    public static void main(String [] args){
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Enter the side of a Square :");
+        int side=scan.nextInt();
 
-    int getPerimeter()
-    {
-        return 4 * side;
-    }
-
-    int getVolume()
-    {
-        return side * side * side;
+        Square sq=new Square(side);
+        System.out.println("For Area enter 1");
+        System.out.println("For Perimeter enter 2");
+        System.out.println("For Volume enter 3");
+        int choice =scan.nextInt();
+        switch(choice){
+            case 1:
+                sq.area();
+                break;
+            case 2:
+                sq.perimeter();
+                break;
+            case 3:
+                sq.volume();
+                break;
+            default:
+                System.out.print("Invalid input");
+                break;
+        }
     }
 }
