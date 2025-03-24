eea616428cf7679c33701a11f3f12621007d3549
Saravana-kumar369 <saravanakumarptk@gmail.com>
2025-03-24 12:32:46 +0530

    Square Classs Changed

--- Square.java
+++ Square.java
@@ -1,3 +1,23 @@
-public class Square {
+import java.util.*;
+class square{
+    int side;
+    square()
+    {
+        side = 0;
+    }
     
+    int getArea()
+    {
+        return side * side;
+    }
+
+    int getPerimeter()
+    {
+        return 4 * side;
+    }
+
+    int getVolume()
+    {
+        return side * side * side;
+    }
 }
