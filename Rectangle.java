a68bd9adbcb568f2249c523941a8c6ba2bbb6172
B-V-GOWTHAM <180419125+B-V-GOWTHAM@users.noreply.github.com>
2025-03-25 08:43:23 +0530

    Success

--- Rectangle.java
+++ Rectangle.java
@@ -1,31 +1,29 @@
 import java.util.*;
-public class Rectangle extends Shape{
-    double length;
-    double breadth;
-    double height;
-    public Rectangle(){
+class Square extends Shape{
+    double side;
+    public Square()
+    {
         Scanner sc=new Scanner(System.in);
-        System.out.print("Enter the length: ");
-        this.length=sc.nextDouble();
-        System.out.print("Enter the breadth: ");
-        this.breadth=sc.nextDouble();
-        System.out.print("Enter the height: ");
-        this.height=sc.nextDouble();
+        System.out.print("Enter the side of the Square: ");
+        this.side=sc.nextDouble();
     }
+
     @Override
-    void getArea(){
-        System.out.println("Area: "+length*breadth);
-    }
-    @Override
-    void getPerimeter(){
-        System.out.println("Perimeter: "+2*(length+breadth));
+    void getArea()
+    {
+        System.out.println("The Area of the Square is "+(side * side));
     }
     
     @Override
-    void getVolume(){
-        System.out.println("Assuming it has cuboid");
-        System.out.println("The Volume of the Cuboid is "+(length * breadth * height));
+    void getPerimeter()
+    {
+        System.out.println("The Perimeter of the Square is "+(4 * side));
+    }
+    
+    @Override
+    void getVolume()
+    {
+        System.out.println("Assuming it has Cube");
+        System.out.println("The Volume of the Cube is "+(side * side * side));
     }
 }
-
-   
